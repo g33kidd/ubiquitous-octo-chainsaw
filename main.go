eaca33e71632f256ed8777bf5f2da8c4ad82c107
Joshua Kidd <kidd.josh.343@gmail.com>
2017-08-31 04:25:56 -0500

    server and channels

--- main.go
+++ main.go
@@ -12,6 +12,46 @@ import (
 	"github.com/nareix/joy4/format/flv"
 )
 
+// The channel type for multiple channel usage ¯\_(ツ)_/¯
+type Channel struct {
+    queue *pubsub.Queue
+}
+
 func main() {
-    fmt.Printf("Hello!")
+
+    // Setup some variables
+    server      := &rtmp.Server{}
+    rwmutex     := &sync.RWMutex{}
+    channels    := map[string] *Channel{}
+
+    // Handles publishing when something comes into the stream ¯\_(ツ)_/¯
+    // TODO: Read the docs on this more...
+    server.HandlePublish = func(conn *rtmp.Conn) {
+        streams, _ := conn.Streams()
+
+        // Lock it to prevent further writes...
+        rwmutex.Lock()
+
+        // The current channel for this stream
+        channel := channels[conn.URL.Path]
+        if channel == nil {
+            channel = &Channel{}
+            channel.queue = pubsub.NewQueue()
+            channel.queue.WriteHeader(streams)
+            channels[conn.URL.Path] = channel
+        } else {
+            channel = nil
+        }
+
+        // Unlock the mutex
+        rwmutex.Unlock()
+
+        if channel == nil {
+            return
+        }
+
+        // Copies the packets that are currently being published.
+        avutil.CopyPackets(channel.queue, conn)
+    }
+
 }
