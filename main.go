3d1be7e94f967eadf9981c5cb58d754241942629
Joshua Kidd <kidd.josh.343@gmail.com>
2017-08-31 04:09:31 -0500

    Stuff

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "sync"
+    "fmt"
+	"io"
+	"net/http"
+	"github.com/nareix/joy4/format"
+	"github.com/nareix/joy4/av/avutil"
+	"github.com/nareix/joy4/av/pubsub"
+	"github.com/nareix/joy4/format/rtmp"
+	"github.com/nareix/joy4/format/flv"
+)
+
+func main() {
+    fmt.Printf("Hello!")
+}
